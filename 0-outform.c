bd1b73edfa8582f7e0a831d8c73f53a22466271d
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-05-23 05:05:41 -0700

    prints length of string

---
+++ 0-outform.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to format
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	printf("That is a very large screen\n", %s);
+
+	while ("%s" != '\0')
+	{
+		printf("strlen([%s])\n");
+	}
+	return (strlen);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+int temp_i(int i);
+int temp_d(int d);
+int convert_binary(int b);
+int convspec_u(int u);
+int convspec_o(int o);
+int convspec_x(int x);
+int convspec_X(int X);
+char call_write(char c);
+char convspec_string(char S);
+
+#endif
